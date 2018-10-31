94c17e707704830711955c03b418250917e291c4
Justin Huffman <justin.t.huffman@gmail.com>
2018-10-31 14:21:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Cities and States Example Project
---
+++ src/main/java/org/wecancodeit/citiesandstates/CitiesAndStatesApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.citiesandstates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesAndStatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesAndStatesApplication.class, args);
+	}
+}
